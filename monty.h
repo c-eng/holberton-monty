6973fd711ad3396d99fa1140df7cc129ef8a15d9
dspham <dsvpham@gmail.com>
2018-09-25 21:28:39 +0000

    updated header - libraries, prototypes

--- monty.h
+++ monty.h
@@ -1,6 +1,15 @@
 #ifndef _MONTY_H_
 #define _MONTY_H_
 
+/*** Libraries ***/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -30,5 +39,7 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/*** Prototypes ***/
 
 #endif /* _MONTY_H_ */
