a165be85bf9bf0a87f6624273e216f0be3e1dac7
dspham <dsvpham@gmail.com>
2018-09-27 01:44:34 +0000

    division, multiplication, modulo opcodes

---
+++ div.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * div - divides the top two elements of the stack
+ * @head: double pointer to head
+ * @line: the line number
+ * Return: nothing
+ */
+void mdiv(stack_t **head, unsigned int line __attribute__((unused)))
+{
+        int div = 0;
+        stack_t *strider;
+
+        if (!*head || (*head)->next == NULL)
+                err_exit(1, "can't div, stack too short\n", 0);
+        strider = (*head)->next;
+	if ((*head)->n == 0)
+		err_exit(1, "division by zero\n", 0);
+	div = (*head)->n / strider->n;
+        strider->n = div;
+        free(*head);
+        strider->prev = NULL;
+        *head = strider;
+}
---
+++ mul.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * mul - multiplies the top two elements of the stack
+ * @head: double pointer to head
+ * @line: the line number
+ * Return: nothing
+ */
+void mul(stack_t **head, unsigned int line __attribute__((unused)))
+{
+        int product = 0;
+        stack_t *strider;
+
+        if (!*head || (*head)->next == NULL)
+                err_exit(1, "can't mul, stack too short\n", 0);
+        strider = (*head)->next;
+        product = (*head)->n * strider->n;
+        strider->n = product;
+        free(*head);
+        strider->prev = NULL;
+        *head = strider;
+}
